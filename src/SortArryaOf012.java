bb55644562b170136160c948a39b5d8b189d9250
rahul-arakka <arakkarahul11@gmail.com>
2023-08-06 16:07:49 +0530

    SortArryaOf012 GFG Solution - JAVA

---
+++ src/SortArryaOf012.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class SortArryaOf012 {
+    public static void sort012(int a[], int n) {
+        // code here
+        Arrays.sort(a);
+        // Linear Search : Not the Efficient Method to sort the array.
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n-i-1;j++){
+        //         if(a[j]>a[j+1]){
+        //             swap(a,j);
+        //         }
+        //     }
+        // }
+    }
+    // public static void swap(int[] a, int j){
+    //     int temp = a[j];
+    //     a[j] = a[j+1];
+    //     a[j+1] = temp;
+    // }
+}
